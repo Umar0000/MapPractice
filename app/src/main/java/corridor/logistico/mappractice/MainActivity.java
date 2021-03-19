057537ca97451e9e9cf0b1ed96d32c4f794647d7
Umar0000 <56391068+Umar0000@users.noreply.github.com>
2021-03-19 15:50:02 +0500

    update

--- app/src/main/java/corridor/logistico/mappractice/MainActivity.java
+++ app/src/main/java/corridor/logistico/mappractice/MainActivity.java
@@ -1,6 +1,7 @@
 package corridor.logistico.mappractice;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -15,14 +16,10 @@ import android.provider.Settings;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-
-import com.github.bkhezry.extramaputils.builder.ViewOptionBuilder;
-import com.github.bkhezry.extramaputils.model.ViewOption;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -34,47 +31,55 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity implements  OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
 
-    private FusedLocationProviderClient mlocationClient;
+    private static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 1;
+    private static final int PlayServiceCode = 10;
+    private final int GPSRequestCode = 101;
+    private FusedLocationProviderClient location_client;
     private GoogleMap mMap;
-
     private MapView mapView;
-    private TextView latlngText;
-    private TextView latlngText2;
+    private TextView textView_camera;
+    private TextView textView_click;
 
+
+    // when activity created...
+    @SuppressLint("VisibleForTests")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
         initMap();
-        // Gets the MapView from the XML layout and creates it
-        mapView = (MapView) findViewById(R.id.map_fragment);
+        mapView =  findViewById(R.id.map_fragment);
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
-        mlocationClient = new FusedLocationProviderClient(Objects.requireNonNull(this).getApplicationContext());
+        location_client = new FusedLocationProviderClient(this
+                .getApplicationContext());
 
     }
 
     private void init() {
-        latlngText=findViewById(R.id.latlngText);
-        latlngText2=findViewById(R.id.latlngText2);
+        textView_camera = findViewById(R.id.latlngText);
+        textView_click = findViewById(R.id.latlngText2);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setMyLocationButtonEnabled(false);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -84,37 +89,30 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mMap.setMyLocationEnabled(true);
-        getlocation();
 
+        mMap.setMyLocationEnabled(true);
+        getLocation();
         mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onCameraIdle() {
                 //get latlng at the center by calling
                 LatLng midLatLng = mMap.getCameraPosition().target;
-                latlngText.setText("Latitude :"+midLatLng.latitude +"\nLongitude :"+ midLatLng.longitude);
+                textView_camera.setText(getResources().getResourceName(R.string.lat) + midLatLng.latitude + "\n" +getResources().getResourceName(R.string.lng)+ midLatLng.longitude);
             }
         });
 
         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onMapClick(LatLng latLng) {
-                latlngText2.setText("Latitude :"+latLng.latitude +"\nLongitude :"+ latLng.longitude);
-
-                Toast.makeText(MainActivity.this,"click",Toast.LENGTH_LONG).show();
-
+                //get R.string.lat text  from string file
+                textView_click.setText(getResources().getResourceName(R.string.lat) + latLng.latitude + "\n" +getResources().getResourceName(R.string.lng)+ latLng.longitude);
             }
         });
-
-
     }
 
-
-
-
-
-
-    private void getlocation() {
+    private void getLocation() {
         if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -125,7 +123,7 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        mlocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+        location_client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
             @Override
             public void onComplete(@NonNull Task<Location> task) {
                 if (task.isSuccessful()) {
@@ -135,38 +133,18 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
                         CameraPosition cameraPosition3_ = new CameraPosition.Builder().target(l).zoom(13).build();
                         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(l, cameraPosition3_.bearing));
                         mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition3_));
-
-                        new ViewOptionBuilder()
-                                .withStyleName(ViewOption.StyleDef.RETRO)
-                                .withCenterCoordinates(l)
-                                .withMarkers(AppUtils.getListExtraMarker())
-                                .withPolygons(
-                                        AppUtils.getPolygon_1()
-                                )
-                                .withPolylines(
-                                        AppUtils.getPolyline_2(),
-                                        AppUtils.getPolyline_4()
-                                )
-                                .withForceCenterMap(false).build();
                     } catch (Exception e) {
                         e.printStackTrace();
                         Log.d("TAG", "onComplete: " + e.getMessage());
-
                     }
-                } else {
-
-//                    Custom_Toast.showToast(getActivity(), "Location Not Found");
-
                 }
             }
         });
     }
 
-
     private void initMap() {
-
         if (isServiceOk()) {
-            if (ischeckpermission()) {
+            if (isCheckPermission()) {
                 if (isGpsEnable()) {
 
                 }
@@ -177,7 +155,6 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
             Toast.makeText(this, "Please Allow to permission to show your location", Toast.LENGTH_SHORT).show();
 
         }
-
     }
 
     private boolean isServiceOk() {
@@ -186,7 +163,7 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
         if (result == ConnectionResult.SUCCESS) {
             return true;
         } else if (googleApiAvailability.isUserResolvableError(result)) {
-            Dialog dialog = googleApiAvailability.getErrorDialog(this, result, K.PlayServiceCode, new DialogInterface.OnCancelListener() {
+            Dialog dialog = googleApiAvailability.getErrorDialog(this, result, PlayServiceCode, new DialogInterface.OnCancelListener() {
                 @Override
                 public void onCancel(DialogInterface Task) {
                     Toast.makeText(MainActivity.this, "update cancel by user", Toast.LENGTH_SHORT).show();
@@ -201,36 +178,21 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
     }
 
     private void requestPermission() {
-//        ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED &&
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
-                && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-//            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE}, K.REQUEST_ID_MULTIPLE_PERMISSIONS);
-//            }
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_ID_MULTIPLE_PERMISSIONS);
+            }
         }
     }
 
-    private boolean ischeckpermission() {
+    private boolean isCheckPermission() {
         int permissionLocation = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
-        int permissionStorage = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
-        int permissionStorage1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
         List<String> listPermissionsNeeded = new ArrayList<>();
-
-
         if (permissionLocation != PackageManager.PERMISSION_GRANTED) {
             listPermissionsNeeded.add(Manifest.permission.ACCESS_FINE_LOCATION);
         }
-        if (permissionStorage != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-        }
-        if (permissionStorage1 != PackageManager.PERMISSION_GRANTED) {
-            listPermissionsNeeded.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
-        }
-
         if (!listPermissionsNeeded.isEmpty()) {
-            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), K.REQUEST_ID_MULTIPLE_PERMISSIONS);
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), REQUEST_ID_MULTIPLE_PERMISSIONS);
             return false;
         }
         return true;
@@ -239,12 +201,10 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == K.REQUEST_ID_MULTIPLE_PERMISSIONS) {
+        if (requestCode == REQUEST_ID_MULTIPLE_PERMISSIONS) {
             Map<String, Integer> perms = new HashMap<>();
 //            perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
             perms.put(Manifest.permission.ACCESS_FINE_LOCATION, PackageManager.PERMISSION_GRANTED);
-            perms.put(Manifest.permission.READ_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
-            perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
             Toast.makeText(this, "OnPermission", Toast.LENGTH_SHORT).show();
             if (grantResults.length > 0) {
                 for (int i = 0; i < permissions.length; i++)
@@ -252,9 +212,7 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
                 // Check for both permissions
 //                perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
 //                        &&
-                if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                        && perms.get(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
-                        && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                if (perms.get(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                     initMap();
 //                        finish();
                     //else any one or both the permissions are not granted
@@ -278,7 +236,6 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
         }
     }
 
-
     private boolean isGpsEnable() {
         LocationManager locationManager = null;
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -289,26 +246,19 @@ public class MainActivity extends AppCompatActivity implements  OnMapReadyCallba
             if (providerEnable) {
                 return true;
             } else {
-                android.app.AlertDialog dialog = new android.app.AlertDialog.Builder(this).setTitle("GPS Permission").setMessage("GPS Is Required For This APPlication..Please Enable Your GPS.").setPositiveButton("YEs", new DialogInterface.OnClickListener() {
+        new android.app.AlertDialog.Builder(this).setTitle("GPS Permission").setMessage("GPS Is Required For This APPlication..Please Enable Your GPS.").setPositiveButton("YEs", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                        startActivityForResult(i, K.GPSRequestCode);
-
+                        startActivityForResult(i, GPSRequestCode);
                     }
                 }).setCancelable(false).show();
             }
-
         } catch (Exception e) {
             e.getMessage();
         }
-
         return false;
     }
-
-
-
-
 
     @Override
     public void onResume() {
